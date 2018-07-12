947a8b46a999aaf0afb84fb8806eb85ce42bdf9b
s41173 <sanjaya.kiran@gmail.com>
2018-07-12 14:42:26 +0700

    background screen

--- www/js/routing.js
+++ www/js/routing.js
@@ -1,5 +1,30 @@
 document.addEventListener('deviceready', function () {
 
+
+  // location
+// onSuccess Callback
+    //   This method accepts a `Position` object, which contains
+    //   the current GPS coordinates
+    //
+    function onSuccess(position) {
+      var element = document.getElementById('geolocation');
+      element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                          'Longitude: ' + position.coords.longitude     + '<br />' +
+                          '<hr />'      + element.innerHTML;
+  }
+
+  // onError Callback receives a PositionError object
+  //
+  function onError(error) {
+      alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+  }
+
+  // Options: throw an error if no update is received every 30 seconds.
+  //
+  var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+  // location
+
   // onesignal
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
@@ -79,10 +104,7 @@ function bgmodeon(type){
 
 function notif_interval(){
   setInterval(function(){ 
-    window.plugins.OneSignal
-    .startInit("95b026cd-a68a-42e4-bcb3-63d5bd93f341")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
+     
      toast('set position');
   }, 10000);
 }
